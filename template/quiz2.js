24f6b21ed52006fd9ddb8de89da3d60a86bc0394
tmb84 <taylor7brown@gmail.com>
2016-03-28 22:30:23 -0400

    changes made to js

--- template/quiz2.js
+++ template/quiz2.js
@@ -1,39 +1,64 @@
-(function($){
-	// This is where you will write your function for the programming challenge
-	// Do not commit console.log statements
-	// Use ajax to reach the api endpoint
-	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js">
+</script>
 
-	$mouseover.on('mouseover', function() {
-		$this = $(this);
-		$(this).html('Scrooge McDuck!');
-		$(this).height($(this).height() + 50);
-	});
+<script type = "text/javascript">
+     $(function(){
+	     $("input[type = submit], a, button").button();
+	 })
+     function request(){
+     var xhttp = new XMLHttpRequest();
+     xttp.open("GET", "http://www.mattbowytz.com/simple_api.json",true);
+     xttp.send();
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
-		return false;
-	});
+ }
 
-	$submit.on('submit', function(e) {
-		e.preventDefault();
-		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
-			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
-		}
-	});
+     $(function(){
+	     // This is where you will write your function for the programming challenge
+	     // Do not commit console.log statements
+	     // Use ajax to reach the api endpoint
+	     // Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
-		}, 1000);
-	});
+	     $mouseover = $('.mouseover');
+	     $click     = $('.click');
+	     $sub       = $('.submit');
 
-})(jQuery);
+
+
+	     $(".mouse-over").mouseover(function() {
+		     
+		     $(this).html('Scrooge McDuck!');
+		     $(this).height($(this).height() + 50);
+		 });
+
+	     $("click.click").click(function() {
+		     $(this).hmtl('Peace Out!')
+			 $(this).fadeout(1500);
+		     return false;
+		 })
+
+		 $(".submit").on('submit', function(e) {
+			 e.preventDefault();
+			 if ($(this).find('input[type="text"]').val() !== '') {
+			     $(this).find('input').foreach(function() {
+				     $(this).fadeout('slow');
+				 });
+			     $(this).appendwith("<h2>Congratulations! You've entered some text!</h2>");
+			 }
+		     });
+	     $(document).on(ready, function() {
+		     setTimeout({
+			     $timeout.fadeIn('slow');
+			 }, 1000);
+		 });
+	 })
+
+	 
+
+</script>
+</head>
+<body>
+	 <button> Get title</button>
+
+</body>
+</html>
